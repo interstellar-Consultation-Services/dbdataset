16387c134577f4e29387624e8dd424825cb60ea6
Mohammed Ali <mali@uplandsoftware.com>
2019-04-27 05:42:56 +0200

    add package documentation

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' DrugBank parsed dataset.
+#'
+#' A dataset containing 74 different datasets parsed
+#' from DrugBank xml database using dbparser r package
+#' \url{https://dainanahan.github.io/dbparser/index.html}.
+#' The package version is copying with DrugBank database
+#'
+#' @format 74 different dataframes contianing different drugs
+#' retlated data
+#' @source \url{https://www.drugbank.ca}
+"dbdataset"
