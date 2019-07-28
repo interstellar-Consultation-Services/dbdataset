01a039854177e965c35c926681c28abec6ea5334
Mohammed Ali <mali@uplandsoftware.com>
2019-07-28 21:14:44 +0200

    add 5.1.4 raw data

--- R/data.R
+++ R/data.R
@@ -5,7 +5,7 @@
 #' \url{https://dainanahan.github.io/dbparser/index.html}.
 #' The package version is copying with DrugBank database
 #'
-#' @format 74 different dataframes contianing different drugs
+#' @format 75 different dataframes contianing different drugs
 #' retlated data
 #' @source \url{https://www.drugbank.ca}
 NULL
